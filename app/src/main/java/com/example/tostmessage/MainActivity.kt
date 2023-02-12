0283d947eb97760555b860c69701e2dacd481130
Veer <veerbahadursen@gmail.com>
2023-02-12 22:55:09 +0530

    My first Commit

--- app/src/main/java/com/example/tostmessage/MainActivity.kt
+++ app/src/main/java/com/example/tostmessage/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.tostmessage
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonUpload=findViewById<Button>(R.id.BtnUpload)
+
+        val buttonDownload=findViewById<Button>(R.id.BtnDownload)
+
+        buttonUpload.setOnClickListener{
+            Toast.makeText(applicationContext,"Uploading...",Toast.LENGTH_SHORT).show()
+        }
+
+        buttonDownload.setOnClickListener{
+            Toast.makeText(applicationContext,"Downloading...",Toast.LENGTH_SHORT).show()
+        }
+
+
+
+
     }
 }
